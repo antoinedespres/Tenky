524cad5bdc86bd69cd2c865284a2b8b81d9a4f34
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2021-03-07 16:25:02 +0100

    Started SharedPreferences implementation
    Co-Authored-By: OnlyNyZe <61805421+OnlyNyZe@users.noreply.github.com>

---
+++ app/src/main/java/fr/dutapp/tenky/SettingsFragment.java
@@ -0,0 +1,31 @@
+package fr.dutapp.tenky;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreferenceCompat;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+
+    }
+}
